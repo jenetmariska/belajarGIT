9628caf315cbe72bdc6722554b5fedc14700bf6a
jenetmariska <164618833+jenetmariska@users.noreply.github.com>
2024-04-23 17:17:04 +0800

    Update style.js

--- style.js
+++ style.js
@@ -2,3 +2,12 @@
     document.querySelector('.header-text').addEventListener('click', function() {
         alert('Anda mengklik judul header!');
     });
+
+ // Fungsi untuk menampilkan pesan konfirmasi
+    function showConfirmation(artikel) {
+        var confirmation = confirm('Anda akan membuka artikel: ' + artikel + '. Lanjutkan?');
+        if (!confirmation) {
+            // Mencegah navigasi jika pengguna membatalkan konfirmasi
+            event.preventDefault();
+        }
+    }
