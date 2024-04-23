1a07325c7ae99c465f1fc16b087f66e9671dc94e
jenetmariska <164618833+jenetmariska@users.noreply.github.com>
2024-04-23 17:09:08 +0800

    Create style.js

---
+++ style.js
@@ -0,0 +1,4 @@
+ // penambahan interaktivitas dengan JavaScript
+    document.querySelector('.header-text').addEventListener('click', function() {
+        alert('Anda mengklik judul header!');
+    });
